c59047a3c66c91b0f003aaca6437d12e9de9b00f
cpereira <cpereira@cpereira-550P5C>
2016-01-24 00:26:05 -0200

    Primeira publicação.

---
+++ src/br/com/main/MainRecursivo.java
@@ -0,0 +1,23 @@
+package br.com.main;
+
+public class MainRecursivo {
+
+	public static void main(String[] args) {
+		System.out.println("o resultado de 2 elevado à 3 é: " + calculaPotenciaRecursivo(2, 3));
+		
+	}
+	
+	
+	public static int calculaPotenciaRecursivo(int base, int expoente) {
+		if(expoente>1)
+			return base * calculaPotenciaRecursivo(base, expoente-1);
+		if(expoente==1)
+			return base;
+		return 0;
+	}
+
+	
+	public static int calculaPotencia(int base, int expoente) {
+		return (int) Math.pow(base, expoente);
+	} 
+}
